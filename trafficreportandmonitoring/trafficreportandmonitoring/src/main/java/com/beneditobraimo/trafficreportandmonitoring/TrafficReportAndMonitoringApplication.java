9d931f5373e81e9dc0e0f690a35e0866b4c1f8a4
BeneditoBraimo <beneditobraimo@gmail.com>
2023-08-14 12:11:08 +0200

    added initial project demo files generated py start.spring.io

---
+++ trafficreportandmonitoring/trafficreportandmonitoring/src/main/java/com/beneditobraimo/trafficreportandmonitoring/TrafficReportAndMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.beneditobraimo.trafficreportandmonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficReportAndMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrafficReportAndMonitoringApplication.class, args);
+	}
+
+}
---
+++ trafficreportandmonitoring/trafficreportandmonitoring/src/test/java/com/beneditobraimo/trafficreportandmonitoring/TrafficReportAndMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beneditobraimo.trafficreportandmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficReportAndMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
